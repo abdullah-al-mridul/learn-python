a75e92506ec882258c4dd4966ff388aad39c5e31
Abdullah Al Mridul <abdullah.github@zohomail.com>
2025-12-05 13:28:30 +0600

    number types

--- learn_py.py
+++ learn_py.py
@@ -99,3 +99,23 @@ def myfunc():
   x = "fantastic"   
 myfunc()
 print("Python is " + x)
+
+
+# numbers
+x = 1    # int
+y = 2.8  # float
+z = 1j   # complex
+
+a = float(x)
+
+b = int(y)
+
+c = complex(x)
+
+print(a)
+print(b)
+print(c)
+
+print(type(a))
+print(type(b))
+print(type(c))
