29a0f0315abc2fd060121525b14d3679ba79a59e
Abdullah Al Mridul <abdullah.github@zohomail.com>
2025-12-05 13:33:48 +0600

    detail casting

--- learn_py.py
+++ learn_py.py
@@ -117,3 +117,16 @@ print(type(c))
 
 # random number
 print(random.randrange(1, 10))
+
+# details casting
+a = int(1)   # x will be 1
+b = int(2.8) # y will be 2
+c = int("3") # z will be 3
+d = float(1)     # x will be 1.0
+e = float(2.8)   # y will be 2.8
+f = float("3")   # z will be 3.0
+g = float("4.2") # w will be 4.2
+h = str("s1") # x will be 's1'
+i = str(2)    # y will be '2'
+j = str(3.0)  # z will be '3.0'
+print(a, b, c, d, e, f, g, h, i, j)
